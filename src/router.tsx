c17962a87b8f4876198c6ece91434e89b657d40b
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-03 19:19:03 +0100

    router setup created

---
+++ src/router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import HomePage from "./pages/HomePage";
+
+
+export const router = createBrowserRouter([
+    {
+        element: <AppLayout />,
+        children: [
+            { path: "/", element: <HomePage /> }
+        ]
+    }
+]);
